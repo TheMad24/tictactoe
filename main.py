c6d7dab49478131344d151456e2a1bd0191465e2
Mhamed Merabet <mad.merabet.24@gmail.com>
2024-03-02 21:01:41 +0100

    Added checkWin

--- main.py
+++ main.py
@@ -23,3 +23,22 @@ class Player:
         print(self.symbol)
 
 
+def checkWin(board):
+    # rows
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2]:
+            return 1
+    
+    # Coloms
+    for j in range(3):
+        if board[0][j] == board[1][j] == board[2][j]:
+            return 1
+    
+    # Diagnal
+    if board[0][0] == board[1][1] == board[2][2]:
+        return 1
+    
+    if board[0][2] == board[1][1] == board[2][0]:
+        return 1
+    
+
